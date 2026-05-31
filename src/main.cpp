9c0012f28ca97b0d47330d6b58bc6c44ad4862a1
saq1rette <165058154+saq1rette@users.noreply.github.com>
2026-05-31 23:00:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
+
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+
+void printFreq(BST<std::string>& tree);
+
 int main() {
+    BST<std::string> tree;
+
+    makeTree(tree, "war_peace.txt");
+
+    printFreq(tree);
+
+    std::cout << tree.depth() << std::endl;
+
     return 0;
 }
